60cf335dd4949bb4cb648f89a1c3964346883b77
Felipe Marques <felipercmarques@gmail.com>
2018-12-15 23:26:59 -0200

    Update inicio_incompleto.py

--- inicio_incompleto.py
+++ inicio_incompleto.py
@@ -1,49 +1,46 @@
 import json
 import googlemaps
-from math import radians, cos, sin, asin, sqrt
+from math import sqrt
 
+#Le o arquivo json cuspido pelo formulario online
 entrada = json.loads(open(r'C:\Users\felip\Desktop\teste.json').read())
 
 address = entrada["address"]
 number = entrada["number"]
 city = entrada["city"]
 
+#Pega informacao adicional do endereco do proprietario da loja com o GoogleMaps API
 gmaps = googlemaps.Client(key='AIzaSyB1S9WYDMHOuPJR8Vi6rAS9dDfvufwpy_M')
-geocode_result = gmaps.geocode(address+", "+number+", "+city)
+geocode_result = gmaps.geocode(address+" "+number+", "+city)
+state = geocode_result[0]['address_components'][4]['long_name']
 
+#Verifica se o sufixo do cep e rural.
+cep = geocode_result[0]['address_components'][6]['long_name']
+area_rural = False
+if cep[-3:] == '899':
+	area_rural = True
 
-def haversine(lon1, lat1, lon2, lat2):
-    """
-    Calculate the great circle distance between two points 
-    on the earth (specified in decimal degrees)
-    """
-    # convert decimal degrees to radians 
-    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
-
-    # haversine formula
-    dlon = lon2 - lon1 
-    dlat = lat2 - lat1 
-    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-    c = 2 * asin(sqrt(a)) 
-    r = 6371 # Radius of earth in kilometers. Use 3956 for miles
-    return c * r
-
-center_point = [{'lat': -7.7940023, 'lng': 110.3656535}]
-test_point = [{'lat': -7.79457, 'lng': 110.36563}]
-
-lat1 = center_point[0]['lat']
-lon1 = center_point[0]['lng']
-lat2 = test_point[0]['lat']
-lon2 = test_point[0]['lng']
-
-radius = 1.00 # in kilometer
-
-a = haversine(lon1, lat1, lon2, lat2)
-
-print('Distance (km) : ', a)
-if a <= radius:
-    print('Inside the area')
-else:
-    print('Outside the area')
+def address_valid():
+	#latitude e longitude da loja do proprietario
+	lat_client = geocode_result[0]['geometry']['location']['lat']
+	lng_client = geocode_result[0]['geometry']['location']['lng']
 	
-	AIzaSyB1S9WYDMHOuPJR8Vi6rAS9dDfvufwpy_M
+	#latitude e longitude do centro da cidade
+	citycode_result = gmaps.geocode("Centro ,"+city+", "+state)
+	lat_centro = citycode_result[0]['geometry']['location']['lat']
+	lng_centro = citycode_result[0]['geometry']['location']['lng']
+
+	radius = 1.00 # em km
+
+	city_point = [{'lat': lat_centro, 'lng': lng_centro}]
+	test_point = [{'lat': lat_client, 'lng': lng_client}]
+
+	a = city_point[0]['lat'] - test_point[0]['lat']
+	b = city_point[0]['lng'] - test_point[0]['lng']
+	c = sqrt(a * a  +  b * b)
+	
+	if c <= radius:
+			return("dentro")
+	else:
+			return("fora")
+			
